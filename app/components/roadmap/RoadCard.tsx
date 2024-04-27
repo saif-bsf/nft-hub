3cee466000a2b5a47a5b9a596746d43a4990d15c
saif <saif.s@bsfdv.com>
2024-04-27 14:16:38 +0400

    feat: road map component

---
+++ app/components/roadmap/RoadCard.tsx
@@ -0,0 +1,29 @@
+import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
+
+type RoadCardProps = {
+  subTitle: string;
+  title: string;
+  description: string;
+  roadCardList: Array<string>;
+};
+const RoadCard = (props: RoadCardProps) => {
+  const { subTitle, title, description, roadCardList } = props;
+  return (
+    <Box className="flex flex-1 basis-1/4 flex-col border rounded-2xl p-8 gap-5">
+      <Typography className="font-bold text-sm">{subTitle}</Typography>
+      <Typography className="font-bold text-xl">{title}</Typography>
+      <Typography>{description}</Typography>
+      <Divider className="max-w-20" />
+      <Box>
+        {roadCardList.map((item) => (
+          <Typography className="my-2" key={item}>
+            {item}
+          </Typography>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default RoadCard;
--- app/components/roadmap/Roadmap.tsx
+++ app/components/roadmap/Roadmap.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import data from "@/app/data/data.json";
 import Heading from "../basic-components/Heading";
+import RoadCard from "./RoadCard";
 
 const Roadmap = () => {
   const { roadMap } = data;
@@ -9,6 +10,17 @@ const Roadmap = () => {
   return (
     <Box>
       <Heading title={title} />
+      <Box className="flex flex-wrap gap-8">
+        {roadCards.map((roadCard) => (
+          <RoadCard
+            key={roadCard.title}
+            roadCardList={roadCard.roadCardList}
+            title={roadCard.title}
+            description={roadCard.description}
+            subTitle={roadCard.subTitle}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
