d759a495f63f14d9707401e3ec03593b672b20a0
saif <saif.s@bsfdv.com>
2024-04-28 10:32:38 +0400

    fix: layout structure

--- app/components/stats/StatsItem.tsx
+++ app/components/stats/StatsItem.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Box, Typography } from "@mui/material";
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
-"use client";
-import { Button, Container } from "@mui/material";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Body from "./components/Body";
-import "./globals.css";
-import theme from "./utils/theme";
+import { Box } from "@mui/material";
+import React from "react";
+import MainPoster from "./components/MainPoster";
+import Carousel from "./components/carousel/Carousel";
+import Features from "./components/features/Features";
+import Stats from "./components/stats/Stats";
+import Product5050 from "./components/product5050/Product5050";
+import SubscribeBox from "./components/SubscribeBox";
+import Roadmap from "./components/roadmap/Roadmap";
+import TeamProfile from "./components/team-profile/TeamProfile";
+import QuesAccordion from "./components/ques-accordion/QuesAccordion";
+import Banner from "./components/banner/Banner";
+import Divider from "./basic-components/Divider";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="bg-gradient-to-br from-backgroudGradient via-white to-transparent min-h-screen flex justify-center items-center">
-      <Container maxWidth="xl">
-        <Header />
-        <Body />
-        <Footer />
-      </Container>
-    </div>
+    <Box>
+      <MainPoster />
+      <Divider />
+      <Carousel />
+      <Divider />
+      <Features />
+      <Divider />
+      <Stats />
+      <Divider />
+      <Product5050 />
+      <Divider />
+      <SubscribeBox />
+      <Divider />
+      <Roadmap />
+      <Divider />
+      <TeamProfile />
+      <Divider />
+      <QuesAccordion />
+      <Divider />
+      <Banner />
+    </Box>
   );
-}
+};
+
+export default Page;
